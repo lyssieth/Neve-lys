1865f1dd940622317c2883ca16abb8fcddd2df83
Lyssieth <lys@lys.ee>
2024-07-01 14:47:59 +0300

    Update none-ls configs

--- config/none-ls/none-ls.nix
+++ config/none-ls/none-ls.nix
@@ -2,21 +2,23 @@
   plugins.none-ls = {
     enable = false;
     enableLspFormat = false;
-    updateInInsert = false;
-    onAttach = ''
-      function(client, bufnr)
-          if client.supports_method "textDocument/formatting" then
-            vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              group = augroup,
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format { bufnr = bufnr }
-              end,
-            })
+    settings = {
+      update_in_insert = false;
+      on_attach = ''
+        function(client, bufnr)
+            if client.supports_method "textDocument/formatting" then
+              vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+              vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                  vim.lsp.buf.format { bufnr = bufnr }
+                end,
+              })
+            end
           end
-        end
-    '';
+      '';
+    };
     sources = {
       code_actions = {
         gitsigns.enable = true;
