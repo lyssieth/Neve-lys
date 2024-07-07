95ad3bab74052b7931c2368a4fc9611bd5f2d120
Lyssieth <lys@lys.ee>
2024-07-07 12:58:54 +0300

    Adjust Neocord

--- config/utils/neocord.nix
+++ config/utils/neocord.nix
@@ -8,14 +8,14 @@
       debounce_timeout = 10;
       editing_text = "Editing...";
       enable_line_number = true;
-      logo = "https://repository-images.githubusercontent.com/325421844/ecb73f47-cb89-4ee0-a0fd-9743c2f3569a";
+      logo = "auto";
       logo_tooltip = "NixVim";
       file_assets = null;
       file_explorer_text = "Browsing...";
       git_commit_text = "Committing changes...";
       global_timer = true;
       line_number_text = "Line %s out of %s";
-      log_level = null;
+      log_level = "error";
       main_image = "logo";
       plugin_manager_text = "Managing plugins...";
       reading_text = "Reading...";
