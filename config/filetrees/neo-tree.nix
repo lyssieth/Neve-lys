dde4cef3a23cda3199ed9c6320f93eb47a4a4404
Lyssieth <lys@lys.ee>
2024-07-14 16:38:14 +0300

    Improvements

--- config/filetrees/neo-tree.nix
+++ config/filetrees/neo-tree.nix
@@ -21,44 +21,6 @@
         "<space>" = "none";
       };
     };
+    filesystem.filteredItems.visible = true;
   };
-
-  # keymaps = [
-  #   {
-  #     mode = "n";
-  #     key = "<leader>e";
-  #     action = ":Neotree toggle reveal_force_cwd<cr>";
-  #     options = {
-  #       silent = true;
-  #       desc = "Explorer NeoTree (root dir)";
-  #     };
-  #   }
-  #   {
-  #     mode = "n";
-  #     key = "<leader>E";
-  #     action = "<cmd>Neotree toggle<CR>";
-  #     options = {
-  #       silent = true;
-  #       desc = "Explorer NeoTree (cwd)";
-  #     };
-  #   }
-  #   {
-  #     mode = "n";
-  #     key = "<leader>be";
-  #     action = ":Neotree buffers<CR>";
-  #     options = {
-  #       silent = true;
-  #       desc = "Buffer explorer";
-  #     };
-  #   }
-  #   {
-  #     mode = "n";
-  #     key = "<leader>ge";
-  #     action = ":Neotree git_status<CR>";
-  #     options = {
-  #       silent = true;
-  #       desc = "Git explorer";
-  #     };
-  #   }
-  # ];
 }
--- config/languages/treesitter/treesitter.nix
+++ config/languages/treesitter/treesitter.nix
@@ -1,8 +1,18 @@
-{
+{pkgs, ...}: {
   plugins.treesitter = {
     enable = true;
-    settings.indent.enable = true;
     folding = true;
     nixvimInjections = true;
+
+    grammarPackages = with pkgs;
+      [
+        tree-sitter-grammars.tree-sitter-just
+      ]
+      ++ vimPlugins.nvim-treesitter.passthru.allGrammars;
+
+    settings = {
+      highlight.enable = true;
+      indent.enable = true;
+    };
   };
 }
